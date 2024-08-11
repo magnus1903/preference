18dd95eb2868b57886e70516df329fc8853770d5
magake62 <magake62@gmail.com>
2024-08-11 19:24:22 +0200

    init

--- app/src/main/java/se/home/magnus/preference/edittext/ParsableEditTextPreference.java
+++ app/src/main/java/se/home/magnus/preference/edittext/ParsableEditTextPreference.java
@@ -1,5 +1,6 @@
 package se.home.magnus.preference.edittext;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.res.TypedArray;
@@ -155,6 +156,12 @@ public class ParsableEditTextPreference extends EditTextPreference {
         _isFragmentManagerSet = false;
         _regularExpression = regularExpression;
         _defaultValue = defaultValue;
+    }
+
+    @SuppressLint("RestrictedApi")
+    @Override
+    public void performClick() {
+        super.performClick();
     }
 
     /**
