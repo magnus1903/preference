ab6d0dc0c3a85325b2801f2e130c651fb1a51884
magake62 <magake62@gmail.com>
2024-09-02 18:29:40 +0200

    init

--- app/src/main/java/se/home/magnus/preference/edittext/ParsableEditTextPreference.java
+++ app/src/main/java/se/home/magnus/preference/edittext/ParsableEditTextPreference.java
@@ -161,7 +161,7 @@ public class ParsableEditTextPreference extends EditTextPreference {
                     @Override
                     public void afterTextChanged(Editable source) {
                         setDialogButtonEnabled(DialogInterface.BUTTON_POSITIVE, source.length() > 0);
-                        setDialogButtonEnabled(DialogInterface.BUTTON_NEGATIVE, editText.getText().length() > 0);
+//                        setDialogButtonEnabled(DialogInterface.BUTTON_NEGATIVE, editText.getText().length() > 0);
                     }
                 });
             }
